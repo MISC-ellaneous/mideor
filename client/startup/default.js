9418bdce0df575620f04ed5205eafebe8f7cbc08
Talon Poole <talon.poole@gmail.com>
2014-09-27 11:02:23 -0600

    moved some things into client/startup

--- client/startup/default.js
+++ client/startup/default.js
@@ -1,2 +1,113 @@
 Meteor.startup(function () {
+
+  // Trying to perserve the global namespace.
+  Mideor = {};
+  Mideor.MIDI = false;
+
+  Mideor.debug = true;
+  Mideor.log = function() {
+    if (!Mideor.debug) { return; }
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift("Mideor" + ': ')
+    // Console.log.apply has to be in the context of console.
+    console.log.apply(console, args);
+  }
+
+  // Get the midi device if we can.
+  navigator.requestMIDIAccess()
+    .then(function (m) {
+      // Web MIDI API changed in Chrome 29 here's a catchall.
+      // Chrome < 29:
+      if (typeof m.inputs === 'function') {
+        Mideor.MIDI = m.inputs()[0];
+      } else {
+        // Chrome > 29:
+        Mideor.MIDI = m.inputs.values().next().value;
+      }
+      if (Mideor.MIDI) {
+        Mideor.log(Mideor.MIDI);
+        Mideor.MIDI.onmidimessage = Mideor.playNote;
+      } else {
+        Mideor.log('no midi device.');
+      }
+    });
+    //.fail(function(err) {
+    //  Mideor.log("MIDI Access Failure. Error code: " + err.code );
+    //});
+
+
+  // Save the MIDI notes to the database.
+  Mideor.saveNote = function(message) {
+    // Push events to database.
+    var note = {
+      keys: message.data,
+      time: message.timestamp
+    }
+    Mid.update(mid, {$push: {'notes': note}});
+
+    // Make it composable with other midi message handlers.
+    return message;
+  }
+
+  // Save the MIDI notes to the database.
+  Mideor.playNote = function(message) {
+    var note = message.data;
+    var letter = Mideor.keycodeToNoteLetter[note[1]];
+    Mideor.log(letter, note);
+
+
+    // Make it composable with other midi message handlers.
+    return message;
+  }
+
+  Mideor.playMid = function(mid) {
+    var sequence = new Sequence();
+    sequence.loop = false;
+    sequence.push(letter + ' q');
+    sequence.play();
+  }
+
+  /**
+   * Do a thing as many times as specified.
+   */
+  Mideor.loop = function (times, fn) {
+    var t = times;
+    while (t) {
+      fn(times - t);
+      t--;
+    }
+  }
+
+  /**
+   * Programatically reconstruct this table:
+   * http://computermusicresource.com/midikeys.html
+   *
+   * @return Object Dictionary of keycodes and letter notes.
+   */
+  Mideor.keycodeToNoteLetter = (function() {
+    var enharmonics = [
+      'C',
+      'Db',
+      'D',
+      'Eb',
+      'Fb',
+      'F',
+      'Gb',
+      'G',
+      'Ab',
+      'A',
+      'Bb',
+      'Cb'
+    ];
+    var map = {};
+    Mideor.loop(11, function(i) {
+      var octave = (-2) + i; // this gives us octaves -2 ... 8
+      enharmonics.forEach(function (note) {
+        var lastKeycode = Number(_.last(_.keys(map)))
+        if (!lastKeycode && !(lastKeycode > -1)) { lastKeycode = -1; }
+      map[lastKeycode + 1] = note + octave;
+      });
+    });
+    return map;
+  }());
 });
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,80 +1,9 @@
-// Set a global MIDI variable
-var MIDI = false;
-var tinymusic = Npm.require('tinymusic');
-
-// Save the MIDI notes to the database.
-var saveNote = function(message) {
-  // Push events to database.
-  var note = {
-    keys: message.data,
-    time: message.timestamp
-  }
-  Mid.update(mid, {$push: {'notes': note}});
-
-  // Make it composable with other midi message handlers.
-  return message;
-}
-
-/**
- * Do a thing as many times as specified.
- */
-function loop(times, fn) {
-  var t = times;
-  while (t) {
-    fn(times - t);
-    t--;
-  }
-
-}
-
-/**
- * Programatically reconstruct this table
- * http://computermusicresource.com/midikeys.html
- *
- * @return Object Dictionary of keycodes and letter notes.
- */
-keycodeNoteMap = function() {
-  var enharmonics = [
-    'C',
-    'Db',
-    'D',
-    'Eb',
-    'Fb',
-    'F',
-    'Gb',
-    'G',
-    'Ab',
-    'A',
-    'Bb',
-    'Cb'
-  ];
-  var map = {};
-  loop(11, function(i) {
-    var octave = (-2) + i; // this gives us octaves -2 ... 8
-    enharmonics.forEach(function (note) {
-      var lastKeycode = Number(_.last(_.keys(map)))
-      if (!lastKeycode && !(lastKeycode > -1)) { lastKeycode = -1; }
-      map[lastKeycode + 1] = note + octave;
-    });
-  });
-  return map;
-}
-
-var playNote = function(message) {
-  var note = message.data;
-  console.log(note);
-
-  // Make it composable with other midi message handlers.
-  return message;
-}
-
 /**
  * Template events should be defined here.
  *
  * Docs: http://docs.meteor.com/#template_events
  */
 Template.home.events({
-
   /**
    * Recording
    *   * Determine end time.
@@ -92,16 +21,14 @@ Template.home.events({
       .addClass('green');
 
     // Triggered for every midi input.
-
-    MIDI.onmidimessage = _.compose(playNote, saveNote);
+    Mideor.MIDI.onmidimessage = _.compose(Mideor.playNote, Mideor.saveNote);
     Meteor.setTimeout(function() {
       var mid = null;
-      MIDI.onmidimessage = playNote;
+      Mideor.MIDI.onmidimessage = playNote;
       $('#record')
         .removeClass('green')
         .addClass('red');
     }, 8000);
-
 
   }
 });
@@ -112,34 +39,10 @@ Template.home.events({
  * Docs: http://docs.meteor.com/#template_rendered
  */
 Template.home.rendered = function() {
-  navigator.requestMIDIAccess().then(function (m) {
-    // Web MIDI API changed in Chrome 29 here's a catchall.
-    // Chrome < 29:
-    if (typeof m.inputs === 'function') {
-      MIDI = m.inputs()[0];
-    } else {
-      // Chrome > 29:
-      MIDI = m.inputs.values().next().value;
-    }
-    if (MIDI) {
-      console.log(MIDI);
-
-      MIDI.onmidimessage = playNote;
-
-    } else {
-      console.log('no midi device.');
-      // so disable recording.
-      $('#record')
-        .attr('disabled', 'disabled')
-        .addClass('disabled');
-    }
-  }, function(err) {
-    // Log access failures
-    console.log("MIDI Access Failure. Error code: " + err.code );
-    // and disable recording.
+  if (!Mideor.MIDI) {
     $('#record')
       .attr('disabled', 'disabled')
       .addClass('disabled');
-  });
+  }
 
 };
