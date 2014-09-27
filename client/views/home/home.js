362162c566465d7abc9eb0698fd6470bbe85a6a0
Talon Poole <talon.poole@gmail.com>
2014-09-27 09:32:42 -0600

    add key to note mapping

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,5 +1,72 @@
 // Set a global MIDI variable
 var MIDI = false;
+var tinymusic = Npm.require('tinymusic');
+
+// Save the MIDI notes to the database.
+var saveNote = function(message) {
+  // Push events to database.
+  var note = {
+    keys: message.data,
+    time: message.timestamp
+  }
+  Mid.update(mid, {$push: {'notes': note}});
+
+  // Make it composable with other midi message handlers.
+  return message;
+}
+
+/**
+ * Do a thing as many times as specified.
+ */
+function loop(times, fn) {
+  var t = times;
+  while (t) {
+    fn(times - t);
+    t--;
+  }
+
+}
+
+/**
+ * Programatically reconstruct this table
+ * http://computermusicresource.com/midikeys.html
+ *
+ * @return Object Dictionary of keycodes and letter notes.
+ */
+keycodeNoteMap = function() {
+  var enharmonics = [
+    'C',
+    'Db',
+    'D',
+    'Eb',
+    'Fb',
+    'F',
+    'Gb',
+    'G',
+    'Ab',
+    'A',
+    'Bb',
+    'Cb'
+  ];
+  var map = {};
+  loop(11, function(i) {
+    var octave = (-2) + i; // this gives us octaves -2 ... 8
+    enharmonics.forEach(function (note) {
+      var lastKeycode = Number(_.last(_.keys(map)))
+      if (!lastKeycode && !(lastKeycode > -1)) { lastKeycode = -1; }
+      map[lastKeycode + 1] = note + octave;
+    });
+  });
+  return map;
+}
+
+var playNote = function(message) {
+  var note = message.data;
+  console.log(note);
+
+  // Make it composable with other midi message handlers.
+  return message;
+}
 
 /**
  * Template events should be defined here.
@@ -19,18 +86,17 @@ Template.home.events({
       createdAt: Date.now()
     });
 
+    // Visually indicate recording.
     $('#record')
       .removeClass('red')
       .addClass('green');
 
     // Triggered for every midi input.
-    MIDI.onmidimessage = function(message) {
-      Mid.update(mid, {$push: {'notes': message}});
-    }
 
+    MIDI.onmidimessage = _.compose(playNote, saveNote);
     Meteor.setTimeout(function() {
       var mid = null;
-      MIDI.onmidimessage = null;
+      MIDI.onmidimessage = playNote;
       $('#record')
         .removeClass('green')
         .addClass('red');
@@ -54,10 +120,13 @@ Template.home.rendered = function() {
     } else {
       // Chrome > 29:
       MIDI = m.inputs.values().next().value;
-      console.log(MIDI);
     }
-    // If no device
-    if (!MIDI) {
+    if (MIDI) {
+      console.log(MIDI);
+
+      MIDI.onmidimessage = playNote;
+
+    } else {
       console.log('no midi device.');
       // so disable recording.
       $('#record')
